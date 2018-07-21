59d5a8a3df110658bbb5bcab8eb7d7506b661de0
hamidfawzi97 <35073262+hamidfawzi97@users.noreply.github.com>
2018-07-21 19:46:13 +0200

    Fsha5a 2

--- resources/views/courses.blade.php
+++ resources/views/courses.blade.php
@@ -66,10 +66,12 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 
 
         <div class="container">
+<!--
             <div class="col-md-6 col-md-offset-6" style="margin-bottom:30px; margin-top:30px">
             <input type="text" name="search_text" id="search_text" placeholder="Search" />
             <input type="button" name="search_button" id="search_button">
             </div>
+-->
             <br/>   
             <div class="col-md-3">
                 <ul class="">
