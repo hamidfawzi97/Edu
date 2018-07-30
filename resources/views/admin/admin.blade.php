568d080accfa1f88a4e705ac09c5286a12bbce77
hamidfawzi97 <35073262+hamidfawzi97@users.noreply.github.com>
2018-07-30 13:53:54 +0200

    users linked

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -68,11 +68,13 @@
                         </ul> -->
                     </li>
                     <li class="menu-item-has-children dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Tables</a>
+                        <a href="{{ url('/admin-user')}}"> <i class="menu-icon fa fa-table"></i>Users</a>
+<!--
                         <ul class="sub-menu children dropdown-menu">
                             <li><i class="fa fa-table"></i><a href="tables-basic.html">Basic Table</a></li>
                             <li><i class="fa fa-table"></i><a href="tables-data.html">Data Table</a></li>
                         </ul>
+-->
                     </li>
                     <li class="menu-item-has-children dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-th"></i>Forms</a>
