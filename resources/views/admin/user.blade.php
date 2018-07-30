f765d0db037358cd292b0763b38f766a2e96feb2
Abdelrahman Gamal <abdelrahman_nasser@hotmail.com>
2018-07-30 14:01:36 +0200

    xommit

--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -13,15 +13,15 @@
     <link rel="apple-touch-icon" href="apple-icon.png">
     <link rel="shortcut icon" href="favicon.ico">
 
-    <link rel="stylesheet" href="asset('admin/css/normalize.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/font-awesome.min.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/themify-icons.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/flag-icon.min.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/cs-skin-elastic.css')}}">
-    <link rel="stylesheet" href="asset('admin/css/lib/datatable/dataTables.bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/normalize.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/font-awesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/flag-icon.min.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/cs-skin-elastic.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/css/lib/datatable/dataTables.bootstrap.min.css')}}">
     <!-- <link rel="stylesheet" href="assets/css/bootstrap-select.less"> -->
-    <link rel="stylesheet" href="asset('admin/scss/style.css')}}">
+    <link rel="stylesheet" href="{{asset('admin/scss/style.css')}}">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
